e690795df6d2dd052c3ed36e7fc5064947f69dd8
Beardocracy <travisjbearden@gmail.com>
2020-12-21 16:50:32 -0600

    Added support for unordered lists

--- markdown2html.py
+++ markdown2html.py
@@ -17,13 +17,30 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as md:
         with open(sys.argv[2], 'w') as html:
+            ul_list_start = False
+            is_ulist = False
             for line in md:
-                llist = line.split(' ')
+
                 heading_level = len(line) - len(line.lstrip('#'))
+
+                if line[0] == '-':
+                    is_ulist = True
+                    if ul_list_start is False:
+                        html.write("<ul>\n")
+                        ul_list_start = True
+                    html.write("<li>" + line.lstrip('-').strip() + "</li>\n")
+
+                if is_ulist and line[0] != '-':
+                    ul_list_start = False
+                    is_ulist = False
+                    html.write("</ul>\n")
+
                 if heading_level >= 1 and heading_level <= 6:
                     htmlline = "<h{}>".format(heading_level)
                     htmlline += line.lstrip('#').strip()
                     htmlline += "</h{}>\n".format(heading_level)
                     html.write(htmlline)
 
+            if ul_list_start is True:
+                html.write("</ul>\n")
     exit(0)
