582ce157d2947bfdb8d6e4d8e7a68d9627d02ac5
Beardocracy <travisjbearden@gmail.com>
2020-12-21 14:45:06 -0600

    Removed extra comment

--- markdown2html.py
+++ markdown2html.py
@@ -5,7 +5,6 @@ import os.path
 ''' Converts markdown to HTML '''
 
 if __name__ == "__main__":
-    ''' Part 0 '''
     ''' Check for correct usage '''
     if len(sys.argv) < 3:
         message = "Usage: ./markdown2html.py README.md README.html"
@@ -13,8 +12,7 @@ if __name__ == "__main__":
         exit(1)
     ''' Check if file exists '''
     if not os.path.isfile(sys.argv[1]):
-        message = "Missing " + sys.argv[1]
-        print(message, file=sys.stderr)
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         exit(1)
 
     exit(0)
