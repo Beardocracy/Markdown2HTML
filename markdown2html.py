e0b662c758eda3ce98f99480dc31e56a4d21f836
Beardocracy <travisjbearden@gmail.com>
2020-12-21 14:34:13 -0600

    Added import guard

--- markdown2html.py
+++ markdown2html.py
@@ -4,16 +4,16 @@ import os.path
 
 ''' Converts markdown to HTML '''
 
+if __name__ == "__main__":
+    ''' Part 0 '''
+    ''' Check for correct usage '''
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+    ''' Check if file exists '''
+    if not os.path.isfile(sys.argv[1]):
+        message = "Missing " + sys.argv[1]
+        print(message, file=sys.stderr)
+        exit(1)
 
-''' Part 0 '''
-''' Check for correct usage '''
-if len(sys.argv) < 2:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
-''' Check if file exists '''
-if not os.path.isfile(sys.argv[1]):
-    message = "Missing " + sys.argv[1]
-    print(message, file=sys.stderr)
-    exit(1)
-
-exit(0)
+    exit(0)
